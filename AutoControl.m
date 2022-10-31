b972c9a1c42f03f3a6cbd28ef8920bb5199753ba
Ariya3 <93688113+Ariya3@users.noreply.github.com>
2022-10-31 11:46:06 -0700

    Add files via upload

---
+++ AutoControl.m
@@ -0,0 +1,17 @@
+%% Variables
+%%global myColorSensor
+brick.SetColorMode(1, 2);
+
+brick.displayColor(1);
+
+%% Main Code
+%%myColorSensor = colorSensor(maurice);
+
+%%color = senseColor();
+%%disp (color);
+
+
+%% Functions
+%%function color = senseColor()
+  %%  color = brick.ColorCode(1);
+%%end
